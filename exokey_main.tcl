40381a26ec9cd3d50a004d2720d08a07644c672d
Karl Hiramoto <karl@hiramoto.org>
2014-05-06 19:16:01 +0200

    scripts and images for exokey

---
+++ exokey_main.tcl
@@ -0,0 +1,14 @@
+## Files to load
+set bootstrapFile       "bootstrap.bin"
+set ubootFile           "u-boot.bin"
+set kernelFile          "linux.bin"
+set rootfsFile          "rootfs.ubi"
+
+## board variant
+set boardFamily "sama5d3"
+set board_suffix "ek"
+set target(traceLevel) 2
+
+
+## now call common script
+source exokey_nandflash.tcl
